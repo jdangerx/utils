db6b01c437f9440f2dec2bc4c90262839d2d7394
John Xia <john.danger.xia@gmail.com>
2018-11-28 09:19:16 -0500

    add some more stuff, also make ln better

--- go.sh
+++ go.sh
@@ -1,4 +1,12 @@
-ln -s ~/dotfiles/.bash_profile ~/.bash_profile
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+#! /usr/bin/env bash
+
+set -xe
+
+ln -si ~/dotfiles/.bash_profile ~/.bash_profile
+ln -si ~/dotfiles/.bashrc ~/.bashrc
+ln -si ~/dotfiles/.vimrc ~/.vimrc
+ln -si ~/dotfiles/.gitconfig ~/.gitconfig
+ln -si ~/dotfiles/.i3 ~/.i3
+ln -si ~/dotfiles/.xresources ~/.xresources
+ln -si ~/dotfiles/.xinitrc ~/.xinitrc
+ln -si ~/dotfiles/.ibus-env.sh ~/.ibus-env.sh
