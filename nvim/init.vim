a2e548449f827bc5318fc7302e9495bd994375d2
John Xia <john@columntax.com>
2023-01-12 09:41:22 -0500

    lots of vim updates

--- nvim/init.vim
+++ nvim/init.vim
@@ -2,6 +2,9 @@ setlocal foldmethod=indent
 set nofoldenable
 set foldlevel=420
 
+set autowriteall
+
 set number relativenumber
 
+lua require('config')
 lua require('plugins')
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -2,4 +2,16 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'justinmk/vim-sneak'
   use 'tpope/vim-surround'
+  use 'tpope/vim-sleuth'
+
+  use {
+      'nvim-treesitter/nvim-treesitter',
+      run = ':TSUpdate'
+  }
+  use 'neovim/nvim-lspconfig'
+
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
 end)
