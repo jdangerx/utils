6dd520c0848cdc66c4e56c488769d51b67e08949
Dazhong Xia <dazhong.xia@catalyst.coop>
2023-06-13 12:37:08 -0400

    Clean up neovim + update helix

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -3,17 +3,4 @@ return require('packer').startup(function(use)
   use 'justinmk/vim-sneak'
   use 'tpope/vim-surround'
   use 'tpope/vim-sleuth'
-
-  use {
-      'nvim-treesitter/nvim-treesitter',
-      run = ':TSUpdate'
-  }
-  use 'neovim/nvim-lspconfig'
-
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.0',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
-
-  use "HallerPatrick/py_lsp.nvim"
 end)
