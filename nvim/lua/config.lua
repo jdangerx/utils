bbd956da3fb54fba7358d1359bd153fd1d572159
Dazhong Xia <dazhong.xia@catalyst.coop>
2025-02-20 17:44:33 -0500

    chore: fix vscode vim-sneak issue

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -1,5 +1,21 @@
 vim.g.mapleader = " "
 
+vim.opt.foldmethod = 'indent'
+vim.opt.foldenable = false
+vim.opt.foldlevel = 420
+vim.opt.autowriteall = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.keymap.set('v', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>Y', '"+yg_')
+vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>yy', '"+yy')
+vim.keymap.set('n', '<leader>p', '"+p')
+vim.keymap.set('n', '<leader>P', '"+P')
+vim.keymap.set('v', '<leader>p', '"+p')
+vim.keymap.set('v', '<leader>P', '"+P')
+
 if vim.g.vscode then
-  cmdheight = 0 -- to avoid weird output window popping up on sneak
+  vim.opt.cmdheight =4
 end
